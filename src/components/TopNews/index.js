300b6c95f6c3c48d2209b830f0b26ff948a527a6
Felipe Henrique <felipeh.soares3@gmail.com>
2020-07-27 18:10:07 -0300

    added information on news cards

--- src/components/TopNews/index.js
+++ src/components/TopNews/index.js
@@ -1,9 +1,31 @@
 import React from 'react'
 
+import {
+  parseISO
+} from 'date-fns'
+import { format } from 'date-fns-tz';
+
+import './style.css'
+
 export default ({ data }) => {
+
+  const addedDate = parseISO(data.publishedAt);
+
+  const dateFormat = format(addedDate, 'dd/MM/yyyy HH:mm');
+
   return (
     <div className="tn-wrap">
-      <img src={data.urlToImage} style={{ height: 350 }} />
+      <img src={data.urlToImage} />
+      <div className="type-news">Brasil</div>
+      <div className="info-new-news">
+        <p className="content-news">{data.title}</p>
+
+        <div className="published-date">
+          <time>
+            {dateFormat}
+          </time>
+        </div>
+      </div>
     </div>
   )
 }
