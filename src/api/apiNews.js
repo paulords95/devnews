c5e29f6e62ff4a85e7954c44ebc4d7a2db1e9a77
Paulo Ricardo da Silva <paulords95@gmail.com>
2020-10-01 18:26:08 -0300

    Update apiNews.js

--- src/api/apiNews.js
+++ src/api/apiNews.js
@@ -3,19 +3,16 @@ export default {
 
     if (searchT) {
       const res = await fetch(`http://newsapi.org/v2/everything?q=${searchT}&from=2020-07-29&sortBy=popularity&apiKey=9562d137b5d64895928b55052b075e4d`);
-      const json = await res.json();
-      return json;
+      return await res.json();
     } else {
       const res = await fetch(
         "http://newsapi.org/v2/top-headlines?country=br&apiKey=9562d137b5d64895928b55052b075e4d"
       );
-      const json = await res.json();
-      return json;
+      return await res.json();
     }
   },
   getCategory: async (type) => {
     const res = await fetch(`http://newsapi.org/v2/everything?q=${type}&from=2020-07-29&sortBy=popularity&apiKey=9562d137b5d64895928b55052b075e4d`);
-    const json = await res.json();
-    return json;
+    return await res.json();
   },
 };
